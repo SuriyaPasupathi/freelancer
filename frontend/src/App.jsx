804adc173a625a869c94ad935a735a2e4c79de0b
SuriyaPasupathi <suriyasuri.481@gmail.com>
2025-04-17 03:20:53 -0700

    "bug_fix"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import LoginForm from "./components/auth/LoginForm";
-import SubscriptionPage from "./pages/SubscriptionPage";
-import MyProfile from "./pages/MyProfile";
+import SubscriptionPage from "./pages/subscriptions";
+import MyAccount from "./pages/MyAccount";
 
 const App = () => {
   return (
@@ -22,7 +22,7 @@ const App = () => {
         {/* App Pages */}
         <Route path="/home" element={<Home />} />
         <Route path="/subscription" element={<SubscriptionPage />} />
-        <Route path="/myprofile" element={<MyProfile />} />
+        <Route path="/myaccount" element={<MyAccount />} />
       </Routes>
     </Router>
   );
