3702fcaa08cb934bc5410bfc9e187b7e405d10cf
SuriyaPasupathi <suriyasuri.481@gmail.com>
2025-04-24 02:45:03 -0700

    urls.py

--- backend/core/user/urls.py
+++ backend/core/user/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     RegisterView, 
     CustomLoginView, 
-    UserProfileViewSet, 
     ReviewViewSet, 
     createaccount, 
     CheckProfileStatusView,
